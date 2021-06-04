561d15003073b3ce3c48853a76b17a67a10c294f
Keem Winning <>
2021-06-04 02:48:33 +0000

    Grading System.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,13 +3,13 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = []; 
+let candidateName = ""; 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride"; 
 let candidateAnswer = "";
 let questions = [`Who was the first American woman in space?`, `True or false: 5 kilometer == 5000 meters? `, `(5 +3)2*10= `, `Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2? `, `What is the minimum crew size for the ISS? `] ; 
-let correctAnswers = [`"Sally Ride"`, `"true"`, `"40"`, `"Trajectory"`, `"3"`]; 
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]; 
 let candidateAnswers = []; 
 
 function askForName() { 
@@ -29,17 +29,22 @@ for (let i = 0; i < questions.length; i++){ // 4 should be questions.length
   // store the answer by pushing to the correct answers array 
    candidateAnswer = input.question(questions[i]);
  candidateAnswers.push(candidateAnswer);
- console.log(`Your Answer: ${candidateAnswer} \n Correct Answer: ${correctAnswers[i]}`)
+ //console.log(`Your Answer: ${candidateAnswer} \n Correct Answer: ${correctAnswers[i]}`)
 }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+let numOfCorrectA = 0
+let numOfQuizQ = candidateAnswers.length
 
-//if (candidateAnswer === correctAnswers) {
-  //console.log("correct")
-//} else { console.log("incorrect")}
+for (let i = 0; i < correctAnswers.length; i++)
+
+if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+numOfCorrectA += 1}
+
+console.log(numOfCorrectA / numOfQuizQ * 100 + "%")
 
   let grade; 
   
