208f7aed70a621383c7f9da39b0fd290dccd4c3e
Zakeem Mccaleb <>
2021-06-06 07:00:53 +0000

    Test This

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,8 +8,8 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride"; 
 let candidateAnswer = "";
-let questions = [`Who was the first American woman in space? `, `True or false: 5 kilometer == 5000 meters? `, `(5 +3)2*10= `, `Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2? `, `What is the minimum crew size for the ISS? `] ; 
-let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]; 
+let questions = [`True or false: 5 kilometer == 5000 meters? `, `(5 +3)2*10= `, `Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2? `, `What is the minimum crew size for the ISS? `] ; 
+let correctAnswers = ["true", "40", "Trajectory", "3"]; 
 let candidateAnswers = []; 
 
 function askForName() { 
@@ -22,7 +22,10 @@ candidateName = input.question("What is your name? ")
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer /
   //
-  // candidateAnswer = input.question (question)
+ candidateAnswer = input.question (question)
+    if (candidateAnswer === correctAnswer) {
+  console.log(`Your Answer: ${candidateAnswer} \nCorrect Answer: ${correctAnswer}`)}
+   else { console.log(`Your Answer: ${candidateAnswer} \nCorrect Answer: ${correctAnswer}`)} 
 for (let i = 0; i < questions.length; i++){ // 4 should be questions.length
   // display question[i]
   // ask for feedback - ie input.question...
@@ -44,11 +47,11 @@ for (let i = 0; i < correctAnswers.length; i++)
 if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
 numOfCorrectA += 1}
 
-let winWin = numOfCorrectA / numOfQuizQ * 100 + "%"
+let winWin = numOfCorrectA / numOfQuizQ  * 100 + "%"
 
-if ("80" <= (winWin)) console.log (`>>> Overall Grade: ${winWin} <<<`)
+if (winWin <= "60") console.log (`>>> Overall Grade: ${winWin} <<<`)
   else {
-    console.log(`>>> Status: FAILED <<<`)
+    console.log(`${winWin} >>> Status: FAILED <<<`)
   }
 
 
